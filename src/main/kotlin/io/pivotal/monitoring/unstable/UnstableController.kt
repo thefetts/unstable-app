fdeac7e318dd3e8db46f9a7574f6a1bcab1e8ed8
Jordan Fallon <thefetts@gmail.com>
2018-08-04 08:02:47 -0700

    Add some custom instrumentation

--- src/main/kotlin/io/pivotal/monitoring/unstable/UnstableController.kt
+++ src/main/kotlin/io/pivotal/monitoring/unstable/UnstableController.kt
@@ -1,5 +1,7 @@
 package io.pivotal.monitoring.unstable
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,15 +9,28 @@ import org.springframework.web.bind.annotation.RestController
 class UnstableController {
     val quizScores = hashMapOf("Mark" to 100, "Brad" to 85, "Jordan" to 70)
 
+    @Autowired
+    private lateinit var registry: MeterRegistry
+
     @GetMapping("/api/data/eventually")
     fun getDataEventually(): Map<String, Int> {
-        if (unlucky()) tenSecondPenalty()
+        val timer = registry.timer("custom.timers", "slow.operations", "calculate.data")
+        timer.record({
+            if (unlucky()) tenSecondPenalty()
+        })
         return quizScores
     }
 
     @GetMapping("/api/data/maybe")
     fun getDataMaybe(): Map<String, Int> {
         if (unlucky()) blowUp()
+        return quizScores
+    }
+
+    @GetMapping("/api/data/withCustomMetric")
+    fun getDataWithCustomMetric(): Map<String, Int> {
+        val counter = registry.counter("custom.counters", "slow.integration.calls", "get.data")
+        counter.increment()
         return quizScores
     }
 }
